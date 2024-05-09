a7474f3ab49e96b92ce36af18843ad650062ed47
Rohan GB <114596598+RGB16@users.noreply.github.com>
2024-05-09 19:53:12 +0530

    Update and rename activity20.java to Activity20.java

--- Selenium Activities/Activity20.java
+++ Selenium Activities/Activity20.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class activity20 {
+public class Activity20 {
     public static void main(String[] args) {
 
         WebDriverManager.firefoxdriver().setup();
